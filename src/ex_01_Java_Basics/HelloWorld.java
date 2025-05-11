7f333e9473851fc23be683c271faa81b260c5359
Ashvinsapariya <ashvin_sapariya@yahoo.co.in>
2025-05-11 14:16:38 +0530

    10 april 2025

--- src/ex_01_Java_Basics/HelloWorld.java
+++ src/ex_01_Java_Basics/HelloWorld.java
@@ -3,7 +3,9 @@ package ex_01_Java_Basics;
 public class HelloWorld {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+
+    {
         System.out.println("Hello Ashvin ");
 
 
---
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -0,0 +1,29 @@
+package ex_01_Java_Basics;
+
+public class Lab001_HelloWorld
+{
+
+    public static void main(String[] args) {
+        //singal line comment
+        /**
+         * name: Ashvin
+         * Batch: SDET12X
+         * TIme: 9 to 1
+         *
+         *
+         *
+         *
+         */
+        /**
+         *
+         *
+         * Course : SDET
+         * Duration: 4 Months
+         * Linkin:
+         * Github: https://github.com/Ashvinsapariya
+         */
+
+
+
+    }
+}
