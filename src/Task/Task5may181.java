63dbd62c39653cb08dba9bf9d3593f78a6dc7ec1
Ashvinsapariya <ashvin_sapariya@yahoo.co.in>
2025-05-22 11:07:03 +0530

    Task 18may

---
+++ src/Task/Task5may181.java
@@ -0,0 +1,14 @@
+package Task;
+
+public class Task5may181 {
+    public static void main(String[] args) {
+        int i = 11;
+
+        i = i++ + ++i;
+        //11 + (11 print then add 1 so 12  after prefix 1+12 =13) 11+13
+
+        System.out.println(i);
+
+
+    }
+}
---
+++ src/Task/Task7may183.java
@@ -0,0 +1,18 @@
+package Task;
+
+public class Task7may183 {
+    public static void main(String[] args) {
+
+        int i = 1;
+        int j = 2;
+        int k = 3;
+
+        int m = i-- - j-- - k--;
+      //  int m = 1-2-3
+
+        System.out.println("i=" + i + ", j=" + j + ", k=" + k + ", m=" + m);
+
+        // 0 + 1+2
+
+    }
+}
