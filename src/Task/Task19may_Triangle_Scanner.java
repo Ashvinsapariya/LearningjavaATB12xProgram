f70bebe85ebe287b053a8c12f14053373271cdf7
Ashvinsapariya <ashvin_sapariya@yahoo.co.in>
2025-05-23 23:21:05 +0530

    If Condition

---
+++ src/Task/Task19may_Triangle_Scanner.java
@@ -0,0 +1,31 @@
+package Task;
+import java.util.Scanner;
+
+
+public class Task19may_Triangle_Scanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter 1st side of Triangle:");
+        int n1 = scanner.nextInt();
+       // Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter 2nd side of Triangle:");
+        int n2 = scanner.nextInt();
+      //  Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter 3rd side of Triangle:");
+        int n3 = scanner.nextInt();
+      //  Scanner scanner = new Scanner(System.in);
+
+        if (n1 == n2 && n2 == n3) {
+            System.out.println("Equilateral Triangle");
+        } else if (n1 == n2 || n1 == n3 || n2 == n3) {
+            System.out.println("Isosceles Triangle");
+        } else {
+            System.out.println("Scalene Triangle");
+        }
+
+
+    }
+}
--- src/Task/Task7may183.java
+++ src/Task/Task7may183.java
@@ -1,5 +1,7 @@
 package Task;
 
+import java.util.Scanner;
+
 public class Task7may183 {
     public static void main(String[] args) {
 
@@ -14,5 +16,37 @@ public class Task7may183 {
 
         // 0 + 1+2
 
+    }
+
+    public static class Task19may_Triangle_Scanner {
+
+        public static void main(String[] args) {
+
+            System.out.println("Enter 1st side of Triangle:");
+            Scanner scanner = new Scanner(System.in);
+            int n1 = scanner.nextInt();
+
+            System.out.println(" Enter 2nd side of Triangle:");
+           // Scanner scanner = new Scanner(System.in);
+            int n2 = scanner.nextInt();
+
+            System.out.println(" Enter 3rd side of Triangle:");
+            //Scanner scanner = new Scanner(System.in);
+            int n3 = scanner.nextInt();
+
+            if(n1==n2&&n2==n3)
+            {
+                System.out.println("Equilateral Triangle");
+            } else if(n1==n2||n1==n3||n2==n3)
+            {
+                System.out.println("Isosceles Triangle");
+            }
+            else
+            {
+                System.out.println("Scalene Triangle");
+            }
+
+
+        }
     }
 }
